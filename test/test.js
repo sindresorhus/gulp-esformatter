25dd3ee401f977533a35d5fe77d89be6c1ef2b16
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-06-26 10:50:23 -0300

    Do not use preset when fecthing .esformatter

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 var assert = require('assert');
 var gutil = require('gulp-util');
-var esformatter = require('../index');
+var esformatter = require('../');
 var path = require('path');
 
 it('should format JS', function (cb) {
@@ -20,12 +20,10 @@ it('should format JS', function (cb) {
 });
 
 it('should fetch .esformatter options from path', function (cb) {
-	var stream = esformatter({
-		preset: 'jquery'
-	});
+	var stream = esformatter();
 
 	stream.on('data', function (file) {
-		assert.equal(file.contents.toString(), 'var foo = [ 1,2,3 ]');
+		assert.equal(file.contents.toString(), 'var foo = [1,2,3]');
 		cb();
 	});
 
