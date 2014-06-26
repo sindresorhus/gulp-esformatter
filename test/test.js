1a09630240de6041253b50972633665d934faf01
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-06-26 15:55:00 +0200

    Close GH-3: Fetch options from .esformatter.

---
+++ test/test.js
@@ -0,0 +1,34 @@
+'use strict';
+var assert = require('assert');
+var gutil = require('gulp-util');
+var esformatter = require('../');
+var path = require('path');
+
+it('should format JS', function (cb) {
+	var stream = esformatter({
+		preset: 'jquery'
+	});
+
+	stream.on('data', function (file) {
+		assert.equal(file.contents.toString(), 'var foo = [ 1, 2, 3 ]');
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		contents: new Buffer('var foo=[1,2,3]')
+	}));
+});
+
+it('should fetch .esformatter options from path', function (cb) {
+	var stream = esformatter();
+
+	stream.on('data', function (file) {
+		assert.equal(file.contents.toString(), 'var foo = [1,2,3]');
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		path: path.join(__dirname, '/foo.js'),
+		contents: new Buffer('var foo=[1,2,3]')
+	}));
+});
